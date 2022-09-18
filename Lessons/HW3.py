02a2bae701ff9dbae0cbe1d42d9fb2ddc93227df
Dim <88140962+dismitry@users.noreply.github.com>
2022-09-18 14:12:03 +0300

    Update HW3.py

--- Lessons/HW3.py
+++ Lessons/HW3.py
@@ -37,3 +37,18 @@ elif len(f1) == len(f2):
 	print('Equal')
 else:
 	print('f2')
+# либо
+
+family_1 = input()
+family_2 = input()
+
+f1 = list(family_1.split(","))
+f2 = list(family_2.split(","))
+
+if len(f1) > len(f2):
+    print('First family bigger')
+elif len(f2) > len(f1):
+    print('Second family bigger')
+else:
+    print('Equal')
+
